5ed144a5595c90e5dff45037d2951998043925b4
Amaal Ali <amq.ali@gmail.com>
2017-01-07 19:43:25 +0000

    Methods introduced:
    - method for updating item quality
    - method to check for bree and backstage passes
    -/-

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -6,12 +6,12 @@ class GildedRose
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" #TODO update quality
-        if item.quality > 0 #TODO Guard, cannot decrese below 0
+      if regular_item?(item)
+        # if positive_quality?(item) #item.quality > 0 #TODO Guard, cannot decrese below 0
           if item.name != "Sulfuras, Hand of Ragnaros" #TODO Filter, Special items
-            item.quality = item.quality - 1 #TODO Medthod
+            modify_quality(item)
           end
-        end
+        # end
       else
         if item.quality < 50 #TODO  Brie and Backstage
           item.quality = item.quality + 1 #TODO Quality of cheese
@@ -54,4 +54,71 @@ class GildedRose
 
     end
   end
+
+  def modify_quality(item)
+    item.quality = item.quality - 1 unless at_minimum_quality?(item)
+  end
+
+  def regular_item?(item)
+    !["Aged Brie", "Backstage passes to a TAFKAL80ETC concert"].include?(item.name)
+  end
+
+  def positive_quality?(item)
+    item.quality > 0 #TODO Guard, cannot decrese below 0
+  end
+
+  def at_minimum_quality?(item)
+    !(item.quality > 0)
+  end
+
+  # def legacy_update_quality()
+  #   @items.each do |item|
+  #     if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" #TODO update quality
+  #       if item.quality > 0 #TODO Guard, cannot decrese below 0
+  #         if item.name != "Sulfuras, Hand of Ragnaros" #TODO Filter, Special items
+  #           item.quality = item.quality - 1 #TODO Medthod
+  #         end
+  #       end
+  #     else
+  #       if item.quality < 50 #TODO  Brie and Backstage
+  #         item.quality = item.quality + 1 #TODO Quality of cheese
+  #         if item.name == "Backstage passes to a TAFKAL80ETC concert" #TODO Backstage passes only
+  #           if item.sell_in < 11
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #           if item.sell_in < 6
+  #             if item.quality < 50
+  #               item.quality = item.quality + 1
+  #             end
+  #           end
+  #         end
+  #       end
+  #     end
+  #
+  #     if item.name != "Sulfuras, Hand of Ragnaros" #TODO update sell in
+  #       item.sell_in = item.sell_in - 1
+  #     end
+  #
+  #     if item.sell_in < 0 #TODO WHen zero
+  #       if item.name != "Aged Brie"
+  #         if item.name != "Backstage passes to a TAFKAL80ETC concert"
+  #           if item.quality > 0
+  #             if item.name != "Sulfuras, Hand of Ragnaros"
+  #               item.quality = item.quality - 1
+  #             end
+  #           end
+  #         else
+  #           item.quality = item.quality - item.quality #TODO Backstage passes to a TAFKAL80ETC concert
+  #         end
+  #       else
+  #         if item.quality < 50
+  #           item.quality = item.quality + 1 #TODO Increases qualiity for Brie
+  #         end
+  #       end
+  #     end
+  #
+  #   end
+  # end
 end
